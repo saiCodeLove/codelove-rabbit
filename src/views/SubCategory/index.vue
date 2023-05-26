39880f513ac0e82270116a9000a3765690a6f907
itheima <itheima@itcast.cn>
2023-05-27 02:19:12 +0800

    列表无限加载功能实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -20,12 +20,33 @@ const reqData = ref({
   pageSize: 20,
   sortField: "publishTime",
 });
-const getGoodList = async() => {
+const getGoodList = async () => {
   const res = await getSubCategoryAPI(reqData.value);
-  console.log(res);
+  //   console.log(res);
   goodList.value = res.result.items;
 };
 onMounted(() => getGoodList());
+// tab栏切换回调
+const tabChange = () => {
+  //   console.log("tab栏切换回调", reqData.value.sortField);
+  reqData.value.page = 1;
+  getGoodList();
+};
+
+// 加载更多
+const disabled = ref(false);
+const load = async () => {
+  console.log("被触发了");
+  // 获取下一页数据
+  reqData.value.page++;
+  const res = await getSubCategoryAPI(reqData.value);
+  // 老数据和新数据拼接
+  goodList.value = [...goodList.value, ...res.result.items];
+  // 加载完毕，停止监听
+  if (res.result.items.length === 0) {
+    disabled.value = true;
+  }
+};
 </script>
 
 <template>
@@ -41,12 +62,16 @@ onMounted(() => getGoodList());
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div
+        class="body"
+        v-infinite-scroll="load"
+        :infinite-scroll-disabled="disabled"
+      >
         <!-- 商品列表-->
         <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" />
       </div>
