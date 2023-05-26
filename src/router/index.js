60147e7b425e1da95c40e0f7cd6039b06d5ff332
itheima <itheima@itcast.cn>
2023-05-27 03:23:42 +0800

    整体认识和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Login from "@/views/Login/index.vue";
 import Home from "@/views/Home/index.vue";
 import Category from "@/views/Category/index.vue";
 import SubCategory from "@/views/SubCategory/index.vue";
+import Detail from '@/views/Detail/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -23,6 +24,10 @@ const router = createRouter({
           path: "category/sub/:id",
           component: SubCategory,
         },
+        {
+          path: "detail/:id",
+          component: Detail,
+        },
       ],
     },
     {
@@ -30,9 +35,9 @@ const router = createRouter({
       component: Login,
     },
   ],
-  scrollBehavior(){
-    return {top:0}
-  }
+  scrollBehavior() {
+    return { top: 0 };
+  },
 });
 
 export default router;
--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -14,7 +14,7 @@ onMounted(() => getNewList());
   <HomePanel title="新鲜好物" subTitle="新鲜好物 品质靠谱">
     <ul class="goods-list">
       <li v-for="item in newList" :key="item.id">
-        <RouterLink to="/">
+        <RouterLink :to="`/detail/${item.id}`">
           <img :src="item.picture" alt="" />
           <p class="name">{{ item.name }}</p>
           <p class="price">&yen;{{ item.price }}</p>
