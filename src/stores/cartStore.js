9fb434ef6015219b392d498feba35506eb3b4fcb
itheima <itheima@itcast.cn>
2023-05-29 02:56:27 +0800

    头部购物车删除功能的实现

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -23,7 +23,15 @@ export const useCartStore = defineStore(
         cartList.value.push(goods);
       }
     };
-    return { cartList, addCart };
+    // 删除购物车
+    const delCart = (skuId) => {
+      // 思路：
+      // 1. 找到要删除项的下标值 - splice
+      // 2. 使用数组的过滤方法 - filter
+      const idx = cartList.value.findIndex((item) => skuId === item.skuId);
+      cartList.value.splice(idx, 1);
+    };
+    return { cartList, addCart,delCart };
   },
   {
     persist: true,
