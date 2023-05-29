ec53998c2f7f446132de3c348c518e3a4bd04b9a
itheima <itheima@itcast.cn>
2023-05-29 13:29:13 +0800

    本地-列表购物车的基础数据渲染

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,8 @@ import Login from "@/views/Login/index.vue";
 import Home from "@/views/Home/index.vue";
 import Category from "@/views/Category/index.vue";
 import SubCategory from "@/views/SubCategory/index.vue";
-import Detail from '@/views/Detail/index.vue'
+import Detail from '@/views/Detail/index.vue';
+import CartList from '@/views/CartList/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -27,6 +28,10 @@ const router = createRouter({
         {
           path: "detail/:id",
           component: Detail,
+        },
+        {
+          path: "cartlist",
+          component: CartList,
         },
       ],
     },
