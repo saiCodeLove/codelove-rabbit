78c767756ebb2f760d91c35baf78c62e877b9b3b
itheima <itheima@itcast.cn>
2023-05-30 12:52:02 +0800

    会员中心路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -6,9 +6,12 @@ import Category from "@/views/Category/index.vue";
 import SubCategory from "@/views/SubCategory/index.vue";
 import Detail from "@/views/Detail/index.vue";
 import CartList from "@/views/CartList/index.vue";
-import Checkout from '@/views/Checkout/index.vue'
+import Checkout from "@/views/Checkout/index.vue";
 import Pay from "@/views/Pay/index.vue";
 import PayBack from "@/views/Pay/PayBack.vue";
+import Member from "@/views/Member/index.vue";
+import UserInfo from "@/views/Member/components/UserInfo.vue";
+import UserOrder from "@/views/Member/components/UserOrder.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -47,6 +50,20 @@ const router = createRouter({
         {
           path: "paycallback",
           component: PayBack,
+        },
+        {
+          path: "member",
+          component: Member,
+          children: [
+            {
+              path: "user",
+              component: UserInfo,
+            },
+            {
+              path: "order",
+              component: UserOrder,
+            },
+          ],
         },
       ],
     },
